fb3b5e09b37d2ede5579cacbdfaa2a76d7506ad8
Simon <simon@yard.nl>
2024-11-10 23:10:00 +0100

    (feat): use method args as accepted args

--- src/Attributes/Filter.php
+++ src/Attributes/Filter.php
@@ -9,13 +9,13 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
 class Filter extends Hook
 {
-    public function register(callable $callable): void
+    public function register(callable $callable, int $acceptedArgs = 1): void
     {
         add_filter(
             $this->hookName,
             $callable,
             $this->priority,
-            $this->acceptedArgs
+            $acceptedArgs
         );
     }
 }
